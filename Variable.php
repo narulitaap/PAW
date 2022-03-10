cb614dd9ed06e7df11d1b42b80408159776ccc5e
narulitaap <narulita.arien@gmail.com>
2022-03-10 14:47:47 +0700

    Menambahkan file variable.php

---
+++ Variable.php
@@ -0,0 +1,12 @@
+<?php
+$a = 7;
+$b = 5;
+$c = 3;
+$Penjumlahan = $a + $b;
+$Pengurangan = $a - $c;
+$Perkalian = $c * $b;
+
+echo "Hasil Penjumlahan = $Penjumlahan </br>";
+echo "Hasil Pengurangan = $Pengurangan </br>";
+echo "Hasil Perkalian = $Perkalian </br>";
+?>
